733926b84118de30f4c7923b920637eb4a78259c
FZsiga02 <fzsigmond02@gmail.com>
2022-09-23 11:20:45 +0200

    Webpack gyakorlás

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 import 'bootstrap/dist/js/bootstrap.js'
 import 'bootstrap/dist/css/bootstrap.css'
 import './main.css'
+
+let szamlalo1
+let szamlalo2
+let szamlalo3
+let szamlalo4
+
+function katt1(){
+    szamlalo1++;
+    document.getElementById("text1").textContent(szamlalo1)
+}
+
+function katt2(){
+    szamlalo2++;
+    document.getElementById("text1").textContent(szamlalo2)
+}
+
+function katt3(){
+    szamlalo3++;
+    document.getElementById("text1").textContent(szamlalo3)
+}
+
+function katt4(){
+    szamlalo4++;
+    document.getElementById("text1").textContent(szamlalo4)
+}
+
+function colorChange(){
+    document.body.style.backgroundColor = document.getElementById("color").value;
+}
+
+function init(){
+    document.getElementById("kep1").addEventListener("click", katt1)
+    document.getElementById("kep2").addEventListener("click", katt2)
+    document.getElementById("kep3").addEventListener("click", katt3)
+    document.getElementById("kep4").addEventListener("click", katt4)
+    document.getElementById("color").addEventListener("change", colorChange)
+}
+
+document.addEventListener("DOMContentLoaded", init)
